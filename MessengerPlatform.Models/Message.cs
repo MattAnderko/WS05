25d09db198433f682405af76af02e496679ecc11
Anderko Máté <mate.anderko@stud.uni-obuda.hu>
2023-04-15 10:28:34 +0200

    Created Message class

---
+++ MessengerPlatform.Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerPlatform.Models
+{
+    public class Message
+    {
+        public string Sender { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime SendingDate { get; set; }
+
+        public Message(string sender, string messageContent, DateTime sendingDate)
+        {
+            Sender = sender;
+            MessageContent = messageContent;
+            SendingDate = sendingDate;
+        }
+    }
+}
